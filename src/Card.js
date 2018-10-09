15b3f4ab2cc2583d93fe9356ae2ff739c481f3ec
katerinamakri <kathrin023@gmail.com>
2018-10-09 17:57:40 +0300

    Fixed basic funtionality and UI

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Card extends Component {
+
 	render(){
 		return(
-			<div>
+			<div className="card-container">
+				<h2> {this.props.cardsData.name} </h2>
+				<div className="card-box">
+					<div className="img-container">
+						<img src="http://placekitten.com/200/300" alt="placeholder"/>
+					</div>
+					<div className="card-info">
+						<h2> Description</h2>
+						<p>{this.props.cardsData.text}</p>
+						<h2>Details</h2>
+						<p> Level: {this.props.cardsData.level}</p>
+						<p> Type: {this.props.cardsData.type}</p>
+						<p> Family: {this.props.cardsData.family}</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/ListView.js
+++ src/ListView.js
@@ -4,15 +4,18 @@ import './App.css';
 class ListView extends Component {
 
 	render(){
-		return(
-			<ul className="list" >
-				{this.props.cards.map( (card, index) => (
-					<li key={index} > 
-						<button> Button </button>         				
-					</li> 
-				))}
-			</ul>
 
+		return(
+			<div className="list-container">
+				<ul className="list" >
+					{this.props.cards.map( (card, index) => (
+						<li key={index} > 
+							<p> {card.cardName} </p>
+							<button> {this.props.cardsData.card_type} </button>         				
+						</li> 
+					))}
+				</ul>
+			</div>
 		);
 	}
 
