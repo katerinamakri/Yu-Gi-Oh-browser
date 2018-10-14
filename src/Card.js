590debf014562d6bbfe73d4cca6a774423677f2c
katerinamakri <kathrin023@gmail.com>
2018-10-14 15:11:01 +0300

    Fixed image fetching

--- src/Card.js
+++ src/Card.js
@@ -4,20 +4,28 @@ import './App.css';
 class Card extends Component {
 
 	render(){
+		let imgUrl;
+
+		if (this.props.query === undefined || this.props.query === ''  || this.props.selectedCard.length === 0) {
+			imgUrl='https://www.colburnschool.edu/wp-content/uploads/2018/02/pix-vertical-placeholder.jpg'
+		} else {
+			imgUrl=`http://52.57.88.137/api/card_image/${this.props.query}`
+		}
+
 		return(
 			<div className="card-container">
-				<h2> {this.props.cardsData.name} </h2>
+				<h2> {this.props.selectedCard.name} </h2>
 				<div className="card-box">
 					<div className="img-container">
-						<img src="http://placekitten.com/200/300" alt="placeholder"/>
+						<img src={imgUrl} alt={this.props.query}  className="card-img"/>
 					</div>
 					<div className="card-info">
 						<h2> Description</h2>
-						<p>{this.props.cardsData.text}</p>
+						<p>{this.props.selectedCard.text}</p>
 						<h2>Details</h2>
-						<p> Level: {this.props.cardsData.level}</p>
-						<p> Type: {this.props.cardsData.type}</p>
-						<p> Family: {this.props.cardsData.family}</p>
+						<p> Level: {this.props.selectedCard.level}</p>
+						<p> Type: {this.props.selectedCard.type}</p>
+						<p> Family: {this.props.selectedCard.family}</p>
 					</div>
 				</div>
 			</div>
