cfb4845d178d50a8416099072d9f362359fc64c1
katerinamakri <kathrin023@gmail.com>
2018-10-14 17:21:38 +0300

    Added card-header section

--- src/Card.js
+++ src/Card.js
@@ -4,17 +4,23 @@ import './App.css';
 class Card extends Component {
 
 	render(){
-		let imgUrl;
+		let imgUrl, displayHeader;
 
 		if (this.props.query === undefined || this.props.query === ''  || this.props.selectedCard.length === 0) {
 			imgUrl='https://www.colburnschool.edu/wp-content/uploads/2018/02/pix-vertical-placeholder.jpg'
+			displayHeader= "none";
+
 		} else {
 			imgUrl=`http://52.57.88.137/api/card_image/${this.props.query}`
+			displayHeader = "flex";
 		}
 
 		return(
 			<div className="card-container">
-				<h2> {this.props.selectedCard.name} </h2>
+				<div className="card-header" style={{ display: displayHeader }}>
+					<h2> {this.props.selectedCard.name} </h2>
+					<p> <span>{this.props.selectedCard.card_type}</span></p>
+				</div>
 				<div className="card-box">
 					<div className="img-container">
 						<img src={imgUrl} alt={this.props.query}  className="card-img"/>
@@ -23,9 +29,9 @@ class Card extends Component {
 						<h2> Description</h2>
 						<p>{this.props.selectedCard.text}</p>
 						<h2>Details</h2>
-						<p> Level: {this.props.selectedCard.level}</p>
-						<p> Type: {this.props.selectedCard.type}</p>
-						<p> Family: {this.props.selectedCard.family}</p>
+						<p> Level: <span>{this.props.selectedCard.level} </span></p>
+						<p> Type: <span>{this.props.selectedCard.type} </span></p>
+						<p> Family: <span>{this.props.selectedCard.family}</span></p>
 					</div>
 				</div>
 			</div>
