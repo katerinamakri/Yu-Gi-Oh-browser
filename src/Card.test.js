72ff7ace20d8c5db2efb07b9696b1db6592c8491
katerinamakri <kathrin023@gmail.com>
2018-10-15 22:55:13 +0300

    Added new test

--- src/Card.test.js
+++ src/Card.test.js
@@ -4,14 +4,19 @@ import { shallow } from 'enzyme';
 
 describe('Card', () => {
 
+	it('renders without props', () => {
+		const wrapper = shallow(<Card />);
+		expect(wrapper.html().length > 0).toBe(true)
+	});
+
 	it('should be defined', () => {
-   	expect(Card).toBeDefined();
- 	});
+		expect(Card).toBeDefined();
+	});
  	
- 	it('should have props', () => {
- 		const wrapper = shallow(<Card query={'Infernoid Attondel'} selectedCard={'Infernoid Attondel'} />)
- 		expect(wrapper.props().selectedCard).toBeDefined();
- 	});
+	it('should have props', () => {
+		const wrapper = shallow(<Card query={'Infernoid Attondel'} selectedCard={'Infernoid Attondel'} />)
+		expect(wrapper.props().selectedCard).toBeDefined();
+	});
  
 });
 
