68d8ce73f65dc268436238f4d019a96be26b45cd
katerinamakri <kathrin023@gmail.com>
2018-10-15 20:52:22 +0300

    Removed unnecessary element

--- src/Header.js
+++ src/Header.js
@@ -3,16 +3,12 @@ import './App.css';
 
 class Header extends Component {
 	render(){
-
-		return(
-        <div className="App-header">
-          <button  className="hamburger-button"></button>       
-          <h1 className="App-title">Yu-Gi-Oh Deck Browser</h1>
-        </div>			
+		return (
+			<div className="App-header">     
+				<h1 className="App-title">Yu-Gi-Oh Deck Browser</h1>
+			</div>			
 		);
 	}
-
-
 }
 
 export default Header;
