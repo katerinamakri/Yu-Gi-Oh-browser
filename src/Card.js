9733a952ca4dda599f225cd15a7493e5859a9791
katerinamakri <kathrin023@gmail.com>
2018-10-15 17:04:49 +0300

    Used destructuring for props

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ class Card extends Component {
 	render(){
 		let imgUrl, displayHeader;
 
-		if (this.props.query === undefined || this.props.query === ''  || this.props.selectedCard.length === 0) {
+		if (this.props.query === undefined || this.props.query === ''  ) {
 			imgUrl='https://www.colburnschool.edu/wp-content/uploads/2018/02/pix-vertical-placeholder.jpg'
 			displayHeader= "none";
 
@@ -15,11 +15,13 @@ class Card extends Component {
 			displayHeader = "flex";
 		}
 
+		const { name, card_type, text, level, type, family } = this.props.selectedCard;
+
 		return(
 			<div className="card-container">
 				<div className="card-header" style={{ display: displayHeader }}>
-					<h2> {this.props.selectedCard.name} </h2>
-					<p> <span>{this.props.selectedCard.card_type}</span></p>
+					<h2> {name} </h2>
+					<p> <span>{card_type}</span></p>
 				</div>
 				<div className="card-box">
 					<div className="img-container">
@@ -27,11 +29,11 @@ class Card extends Component {
 					</div>
 					<div className="card-info">
 						<h2> Description</h2>
-						<p>{this.props.selectedCard.text}</p>
+						<p>{text}</p>
 						<h2>Details</h2>
-						<p> Level: <span>{this.props.selectedCard.level} </span></p>
-						<p> Type: <span>{this.props.selectedCard.type} </span></p>
-						<p> Family: <span>{this.props.selectedCard.family}</span></p>
+						<p> Level: <span>{level} </span></p>
+						<p> Type: <span>{type} </span></p>
+						<p> Family: <span>{family}</span></p>
 					</div>
 				</div>
 			</div>
