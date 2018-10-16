f4f0e0ac9a9760112cc22bed5ddb8dc3765de9bd
katerinamakri <kathrin023@gmail.com>
2018-10-16 18:01:48 +0300

    Fixed functionality for hamburger button

--- src/ListView.js
+++ src/ListView.js
@@ -4,9 +4,10 @@ import './App.css';
 class ListView extends Component {
 
 	render(){
+		let displayListView = this.props.isListViewOpen ? "block" : "none";
 
 		return(
-			<div className="list-container">
+			<div className="list-container" style={{ display: displayListView }}>
 				<ul className="list" >
 					{this.props.cards.map( (card, index) => (
 						<li key={index} onClick={() => this.props.fetchCardData(card.cardName)}> 
