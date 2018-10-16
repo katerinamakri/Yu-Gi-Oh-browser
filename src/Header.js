c1103bad815b8eff182d7651dffa03b0f511017d
katerinamakri <kathrin023@gmail.com>
2018-10-16 18:00:21 +0300

    Added hamburger button

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,8 @@ import './App.css';
 class Header extends Component {
 	render(){
 		return (
-			<div className="App-header">     
+			<div className="App-header">
+				<button  className="hamburger-button" onClick={() => this.props.toggleListView()}><i className="fas fa-bars"></i></button>    
 				<h1 className="App-title">Yu-Gi-Oh Deck Browser</h1>
 			</div>			
 		);
