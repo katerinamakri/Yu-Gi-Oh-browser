e3cda0550517379e3f18280c2763ea93b259e7f9
katerinamakri <kathrin023@gmail.com>
2018-11-13 14:45:27 +0200

    Updates

---
+++ precache-manifest.cc916a896fc118e0e54c0468a0f22cfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9b34282d65f6d7a9cd7",
+    "url": "/Yu-Gi-Oh-browser/static/js/runtime~main.b9b34282.js"
+  },
+  {
+    "revision": "4f1270c267136681be63",
+    "url": "/Yu-Gi-Oh-browser/static/js/main.4f1270c2.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/Yu-Gi-Oh-browser/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "4f1270c267136681be63",
+    "url": "/Yu-Gi-Oh-browser/static/css/main.247a901e.chunk.css"
+  },
+  {
+    "revision": "06a3258ab10b5873f575dbdd24565675",
+    "url": "/Yu-Gi-Oh-browser/index.html"
+  }
+];
